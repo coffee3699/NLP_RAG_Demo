daf3386e2733cd8148088a6d04f5b0903bdbf562
高煜博 <91862585+AndyBupt@users.noreply.github.com>
2024-06-17 17:13:32 +0800

    Add files via upload

---
+++ code/Sentence_window_retrieval_llmrerank.sh
@@ -0,0 +1,6 @@
+python Sentence_window_retrieval.py \
+--model_type api \
+--api_key kACKmpESNWbtPqZTnHyFBqnM \
+--secret_key ubSTYNfAxGOJO20cf3d82iwpAAZT7NdY \
+--embedding_model_path ../BAAI/bge-small-en-v1.5/snapshots/5c38ec7c405ec4b44b94cc5a9bb96e735b38267a \
+--data_path '../data/Elon.txt'
